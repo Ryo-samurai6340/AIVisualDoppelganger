77b32a61f577b8097cb4749398469a6890b85391
Ryoichi Homma <131563887+Ryo-samurai6340@users.noreply.github.com>
2024-03-17 19:11:54 +0900

    Revert "Update main.py"
    This reverts commit 13e8c2b1bb723edbb3a0e784ad9cbecd158eed6a.

--- main.py
+++ main.py
@@ -115,4 +115,4 @@ def sendMail():
     
 # Run the Flask app in debug mode
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=os.environ.get('PORT', 5500))
