c06cf508105819934c617ab6415d42cfd1c4f75d
Ryoichi Homma <131563887+Ryo-samurai6340@users.noreply.github.com>
2024-03-18 16:44:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ def process():
     
 # Run the Flask app in debug mode
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.environ.get('PORT', 5500))
+    app.run(debug=True, host="0.0.0.0", port=5500)
