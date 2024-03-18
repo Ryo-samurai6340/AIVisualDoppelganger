60f47696ff8d947a09db053513dfa4d3b946b0cc
Ryoichi Homma <131563887+Ryo-samurai6340@users.noreply.github.com>
2024-03-18 15:01:19 +0900

    Revert "Update script.js"
    This reverts commit 25f04fba3909ea68864a882beffdf119b324c184.

--- static/script.js
+++ static/script.js
@@ -60,7 +60,7 @@ function downloadReplicatedImage() {
 }
 
 // Function to enable users to send message from the contact section
-function sendMail(event) {
+function sendMail() {
     event.preventDefault(); 
     // Validate the form before sending the email
     if (!validateForm()) {
@@ -74,10 +74,9 @@ function sendMail(event) {
         message : document.getElementById("message").value,
     }
     emailjs.send("service_txmsajv", "template_li27qnr", parms).then(function() {
-        document.getElementById("status-message").textContent = "Your enquiry has been sent successfully!";
+        alert("Your enquiry has been sent successfully!");
     }, function(error) {
-        console.error("Failed to send email:", error);
-        document.getElementById("status-message").textContent = "Failed to send email."; 
+        console.error("Failed to send email. Please try again later."); 
     });
 }
 
@@ -92,7 +91,7 @@ function validateForm() {
     // Check if any input field is empty
     if (name === "" || email === "" || subject === "" || message === "") {
         // Display error message
-        document.getElementById("status-message").textContent = "Please fill in all fields.";
+        alert("Please fill in all fields.");
         return false; // Return false to prevent form submission
     }
     return true; // Return true if all fields are filled
