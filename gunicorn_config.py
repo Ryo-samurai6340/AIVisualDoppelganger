c8d830a23b3204e8245c1941bb30ca092ede125d
Ryoichi Homma <131563887+Ryo-samurai6340@users.noreply.github.com>
2024-04-08 16:41:34 +0900

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 import os
 
 bind = "0.0.0.0:" + str(os.environ.get('PORT', 5500))
-workers = 2
-timeout = 3600 * 19
+workers = 1    # scalability (only 1 request is available as of now)
+timeout = 3600 * 20    # 20h (up to 902,500px (total dimension) or 1920px (sum dimension) of image is available)
