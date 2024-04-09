4d4495cc9e81e503f79070ed8347db817b1cf765
Ryoichi Homma <131563887+Ryo-samurai6340@users.noreply.github.com>
2024-04-09 06:08:46 -0700

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,4 +2,4 @@ import os
 
 bind = "0.0.0.0:" + str(os.environ.get('PORT', 5500))
 workers = 2    # concurrency (1 request for replication available at the same time)
-timeout = 3600 * 30    # 20h (up to 1920px (total dimension) of image is available)
+timeout = 3600 * 20    # 20h (up to 1920px (total dimension) of image is available)
